2973529895802fc40cdb8b88bccd6a8760323234
NobbydeNobbes <mortelette.julien@gmail.com>
2022-04-05 11:01:08 +0200

    Catalogue

--- header.php
+++ header.php
@@ -4,6 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./style/main.css">
+    <title>Literie3000</title>
 </head>
 <body>
+    <div class="app-wrapper">
+        <div class="header">
+            <img src="./img/3.png" alt="logo">    
+           <a href="index.php"> <h1>Literie3000</h1> </a>
+            <h2>Catalogue</h2>
+            <form action="">
+                <input type="number" id="sale" name="sale">
+            </form>
+        </div>
+
+    </div>
--- index.php
+++ index.php
@@ -1,6 +1,38 @@
 <?php
+$dsn="mysql:host=localhost;dbname=dodo3000";
+$db= new PDO($dsn, "root", "");
+$query=$db->query("SELECT * FROM beds");
+$items=$query->fetchAll();
+foreach($items as $beds){
+$price=$beds["price"];};
+    if(isset($_GET["sale"])){
+        $price=$price-($_GET["sale"]);
+        
+    }
+
 include("header.php");
 ?>
     
+  <div class="catalogue">
+        
+<?php
+foreach($items as $bed){
+?>  <div class="catalogue-item">
+        <img src="./products/<?=$bed["name"]?>.jpg">
+        <h3><?=$bed["brand"]?></h3>
+        <p><?=$bed["name"]?>  <?=$bed["size"]?></p>
+        
+        <div class="price">
+        
+        <?php if(isset($_GET["sale"])){?>
+            <p><strike><?=$bed["price"]?> € </strike></p>
+             <p> <?= $price ?> </p>
+            <?php }else{?><p><?=$bed["price"]?> € <?php } ?></p>
+        </div>
+
+    </div>
+<?php } ?>
+    
+</div>
 </body>
 </html>
