0f8b9efb50e4899ac0b9e732c1ccfff9219c4305
NobbydeNobbes <mortelette.julien@gmail.com>
2022-04-05 09:11:19 +0200

    firstcommit

---
+++ dodo3000.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE dodo3000;
+USE dodo3000;
+CREATE TABLE beds(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    brand VARCHAR(20),
+    name VARCHAR(50),
+    size VARCHAR(20),
+    price INT
+);
+INSERT INTO beds
+(brand, name, size, price)
+values
+("EPEDA"; "MATELAS Delhi", "90x190", "759");
+INSERT INTO beds
+(brand, name, size, price)
+values
+("DREAMWAY"; "MATELAS Orlando", "90x190", "809");
+INSERT INTO beds
+(brand, name, size, price)
+values
+("BULTEX"; "MATELAS Valenciennes", "140x190", "759");
+INSERT INTO beds
+(brand, name, size, price)
+values
+("EPEDA"; "MATELAS Seville", "160x200", "1019");
